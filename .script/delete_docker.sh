4819568287733988c442adb6f55dc4aabd815f3e
tanknight7 <tanknight@LAPTOP-V0K8ITB3>
2025-04-27 18:11:57 +0700

    Adding fixes

--- .script/delete_docker.sh
+++ .script/delete_docker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 docker stop $(docker ps -q)
 docker rm $(docker ps -a -q)
+docker volume rm $(docker volume ls -q)
 docker rmi -f $(docker images -q)
--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-# docker-entrypoint.sh
-
-# Run Django migrations and makemigrations
 python3 manage.py makemigrations
 python3 manage.py migrate
 
 # Execute the default command to start the Django server
-exec "$@"
+python3 manage.py runserver 0.0.0.0:80
