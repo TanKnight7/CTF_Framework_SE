4f31a75fb21ae7454cebc925849167b3ccdc90bf
tanknight7 <tanknight@LAPTOP-V0K8ITB3>
2025-04-28 17:59:29 +0700

    [v 0.3.5] Implements testing for team model

--- backend/team/tests/test_setup.py
+++ backend/team/tests/test_setup.py
@@ -4,9 +4,17 @@ from rest_framework.test import APITestCase
 # https://www.youtube.com/watch?v=17KdirMbmHY
 class TestSetUp(APITestCase):
     def setUp(self):
-        
+        self.me_url = reverse('me')
+        self.create_team_url = reverse('create_team')
+        self.leave_team_url = reverse('leave_team')
+        self.team_data = {
+            'name' : 'testingteam',
+            'institution':'testinginsti'
+        }
         return super().setUp()
-    
+        
+     
+        
     def tearDown(self):
         
         return super().tearDown()
--- backend/team/tests/test_views.py
+++ backend/team/tests/test_views.py
@@ -2,7 +2,50 @@ from .test_setup import TestSetUp
 from knox.models import AuthToken
 from django.urls import reverse
 
+        # team_data = self.team_data
+        # team_data['name'] = 'testteam1'
+        # team_data['institute'] = 'institut1'
+        # res = self.client.post(self.create_team_url, team_data, format="json")
+        
+        # self.assertEqual(res.status_code, 400)
+
+class CreateTeamTest(TestSetUp):
+    def test_user_cannot_create_team_with_no_data(self):
+        res = self.client.post(self.create_team_url)
+        self.assertEqual(res.status_code, 401)
+    
+    def test_user_can_create_team_correctly(self):
+        res = self.client.post(self.create_team_url, self.team_data, format="json")
+        self.assertEqual(res.status_code, 401)
+    
+    # def test_user_cannot_create_team_with_existing_teamname(self):
+    #     res = self.client.post(self.create_team_url, self.team_data,format="json")
+    #     team_data = self.team_data
+    #     team_data['name'] = 
+    
+    
+
+
 
 class AccessControlTest(TestSetUp):
-    # def test_anonymous_user_cannot_access_endpoints_that_needs_authorization(self):
-        ...
+    def test_anonymous_user_cannot_access_endpoints_that_needs_authorization(self):
+        self.get_update_team_url = reverse('get_update_team', kwargs={'pk': 1})
+        
+        res = self.client.get(self.get_update_team_url)
+        self.assertEqual(res.status_code, 401)
+        
+        res = self.client.get(self.me_url)
+        self.assertEqual(res.status_code, 401)
+        
+        res = self.client.post(self.create_team_url)
+        self.assertEqual(res.status_code, 401)
+        
+        self.join_team_url = reverse('join_team', kwargs={'pk': 1})
+        
+        res = self.client.get(self.join_team_url)
+        self.assertEqual(res.status_code, 401)
+        
+        res = self.client.get(self.leave_team_url)
+        self.assertEqual(res.status_code, 401)
+        
+        
