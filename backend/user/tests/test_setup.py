9a34ff87ee12b4eef31092214994c3b53ef802e4
tanknight7 <tanknight@LAPTOP-V0K8ITB3>
2025-04-28 12:27:04 +0700

    [v 0.3.2] Implements access control testing for get_update_delete_user function

--- backend/user/tests/test_setup.py
+++ backend/user/tests/test_setup.py
@@ -9,12 +9,28 @@ class TestSetUp(APITestCase):
         self.register_url = reverse('register')
         self.login_url = reverse('login')
         
+        self.get_all_users_url = reverse('get_all_users')
+        self.get_update_delete_user_url = None
+        self.me_url = reverse('me')
         # fields = ['username', 'password', 'email', 'roles', 'bio', 'country', 'team']
         self.user_data = {
             'username': 'test',
             'email': 'test@test.com',
             'password': 'Test123@#!user'
         }
+        
+        self.user_data_attacker = {
+            'username': 'attacker',
+            'email': 'attacker@test.com',
+            'password': 'Test123@#!user'
+        }
+        
+        self.user_data_victim = {
+            'username': 'victim',
+            'email': 'victim@test.com',
+            'password': 'Test123@#!user'
+        }
+        
         return super().setUp()
     
     def tearDown(self):
--- backend/user/tests/test_views.py
+++ backend/user/tests/test_views.py
@@ -1,5 +1,6 @@
 from .test_setup import TestSetUp
 from knox.models import AuthToken
+from django.urls import reverse
 
 class RegisterTest(TestSetUp):
     def test_user_cannot_register_with_no_data(self):
@@ -31,7 +32,7 @@ class RegisterTest(TestSetUp):
         role_registered = res.json().get('user').get('role')
         self.assertEqual(role_registered, 'player')
         
-        
+    
 
 class LoginTest(TestSetUp):
     def test_login_and_token_creation(self):
@@ -51,3 +52,40 @@ class LoginTest(TestSetUp):
         # Verify that the old token is no longer in the database
         token_count = AuthToken.objects.all().count()
         self.assertEqual(token_count, 1) # should be 1 token left in the database.
+
+class AccessControlTest(TestSetUp):
+    def test_anonymous_user_cannot_access_endpoints_that_needs_authorization(self):
+        self.get_update_delete_user_url = reverse('get_update_delete_user', kwargs={'pk': 1})
+        
+        res = self.client.get(self.get_all_users_url)
+        self.assertEqual(res.status_code, 401)
+        
+        res = self.client.get(self.get_update_delete_user_url)
+        self.assertEqual(res.status_code, 401)
+        
+        res = self.client.delete(self.get_update_delete_user_url)
+        self.assertEqual(res.status_code, 401)
+        
+        res = self.client.put(self.get_update_delete_user_url, self.user_data, format="json")
+        self.assertEqual(res.status_code, 401)
+        
+        res = self.client.get(self.me_url)
+        self.assertEqual(res.status_code, 401)
+        
+    def test_user_cannot_change_another_user_data(self):
+        # Register accounts
+        self.client.post(self.register_url, self.user_data_attacker, format="json")
+        self.client.post(self.register_url, self.user_data_victim, format="json")
+        
+        user_attacker_token = self.client.post(self.login_url, self.user_data_attacker, format="json").json().get('token')
+        user_victim_id = self.client.post(self.login_url, self.user_data_victim, format="json").json().get('user').get('id')
+        
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + user_attacker_token)
+        self.get_update_delete_user_url = reverse('get_update_delete_user', kwargs={'pk': user_victim_id})
+        
+        res = self.client.delete(self.get_update_delete_user_url)
+        self.assertEqual(res.status_code, 403)
+        
+        res = self.client.put(self.get_update_delete_user_url, self.user_data, format="json")
+        self.assertEqual(res.status_code, 403)
+        
