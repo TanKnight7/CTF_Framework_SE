5873c41c0b3f1f4778f82ad684100d10f291e43a
Tan Knight <49807077+TanKnight7@users.noreply.github.com>
2025-05-15 23:00:31 +0700

    Update delete_docker.sh

--- .script/delete_docker.sh
+++ .script/delete_docker.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-docker stop $(docker ps -q)
-docker rm $(docker ps -a -q)
-docker volume rm $(docker volume ls -q)
+echo "delete_docker.sh changed temporarily for keamanan server chall csc binus wkwk"
 docker rmi -f ctf_framework_se-backend
