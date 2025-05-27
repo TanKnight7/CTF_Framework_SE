748b53532799eb4702de42a79fc70b0c7f195917
Lexxx28 <marvellbudiman@gmail.com>
2025-05-27 21:41:33 +0700

    [V0.3.5.7] Updating writeups models, Save uploaded writeup files using UUID filenames

--- backend/writeup/models.py
+++ backend/writeup/models.py
@@ -1,10 +1,16 @@
+import uuid
+import os
 from django.db import models
 
-# Create your models here.
+def upload_to_uuid(instance, filename):
+    ext = os.path.splitext(filename)[1]  # get file extension
+    new_filename = f"{uuid.uuid4()}{ext}"  # generate UUID filename
+    return os.path.join('wu', new_filename)
+
 class Writeup(models.Model):
     team = models.ForeignKey('team.Team', on_delete=models.CASCADE, null=True, blank=True)
     user = models.ForeignKey('user.User', on_delete=models.CASCADE, null=True, blank=True)
-    path = models.FileField(upload_to='wu/', null=True, blank=True)
+    path = models.FileField(upload_to=upload_to_uuid)  # use the custom function here
     submission_time = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
