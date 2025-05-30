4a099da41a642c640cdb868890b3fa376f99c1d2
tanknight7 <tanknight@LAPTOP-V0K8ITB3>
2025-05-31 03:37:23 +0700

    Updating delete_docker.sh scripts

--- .script/delete_docker.sh
+++ .script/delete_docker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 docker stop softeng_nginx softeng_backend softeng_db
 docker rm softeng_nginx softeng_backend softeng_db
+docker rmi softeng_backend
 docker volume rm ctf_framework_se_postgres_data
