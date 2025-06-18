8402bbb1a779a9b826820427c7bd152d0dda4ab8
Lexxx28 <marvellbudiman@gmail.com>
2025-06-18 15:26:25 +0700

    Fixing Download Bug

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,11 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hns(f(621+#encn1eeqki_=pqihpt2)ve-s(3va2!9puig07&-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-
+ 
 # Application definition
 
 INSTALLED_APPS = [
